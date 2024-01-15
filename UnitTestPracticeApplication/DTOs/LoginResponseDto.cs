db16ed2ab953b030863b366025163b930572e9ab
Svarlione <ruslan.brigockij@gmail.com>
2024-01-15 19:36:21 +0200

    Add project files.

---
+++ UnitTestPracticeApplication/DTOs/LoginResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace UnitTestPracticeApplication.DTOs
+{
+    public class LoginResponseDto : ResponseDto
+    {
+        public string Token { get; set; }
+
+        public LoginResponseDto(bool isSuccess, string message, string token) : base(isSuccess, message)
+        {
+            Token = token;
+        }
+    }
+}
---
+++ UnitTestPracticeApplication/Services/JwtService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace UnitTestPracticeApplication.Services
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetJwtToken(string username)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, username)
+            };
+
+            var secretKey = _configuration.GetSection("Jwt:Key").Value;
+            var issuer = _configuration.GetSection("Jwt:Issuer").Value;
+            var audience = _configuration.GetSection("Jwt:Audience").Value;
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: cred);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
